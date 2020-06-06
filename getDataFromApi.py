f83bb5864b3bd5ef9bd76b905a17298c6c8b998e
Nils <nils.quetschlich@gmail.com>
2020-06-06 22:23:23 +0200

    tested different APIs

--- getDataFromApi.py
+++ getDataFromApi.py
@@ -1,4 +1,6 @@
 import requests
+import pandas as pd
+from bs4 import BeautifulSoup
 
 
 def getKey():
@@ -17,6 +19,22 @@ def retrieveCurrentSharePrice(key):
     response = requests.get(API_URL, data)
     print(response.text)
 
+def getSomeInfoForETF(etf, key):
+    import json
+
+    with open('example_response.json') as f:
+        data = json.load(f)
+
+    print (data)
+
+def parseAvista(isin):
+    response = requests.get("https://www.ariva.de/" + isin + "/zusammensetzung")
+    soup = BeautifulSoup(response.text)
+    print(soup.find(id == "CONTENT"))
+    #print(response.text)
+
 if __name__ == "__main__":
     key = getKey()
-    retrieveCurrentSharePrice(key)
+    #retrieveCurrentSharePrice(key)
+    #getSomeInfoForETF("EUNL", eod_key)
+    parseAvista("IE00B4L5Y983")
