69487372376c139bf9f746832c28379905aa0e61
Nils <nils.quetschlich@gmail.com>
2020-06-07 17:17:25 +0200

    added method to return current portfolio DF

--- portfolioManager.py
+++ portfolioManager.py
@@ -18,6 +18,10 @@ class PortfolioManager:
                 newFileWriter.writerow(['name', 'isin', 'symbol', 'share', 'category'])
             self.df = pd.read_csv("portfolio.csv")
 
+
+    def get_df(self):
+        return self.df
+
     def __init__(self):
         self.read_local_csv()
         #read current share file if available
@@ -27,3 +31,5 @@ if __name__ == '__main__':
     a = PortfolioManager()
     a.add_item("Lufthansa", "DE0008232125", "LHA.FRK", "10", "stock")
     a.add_item("Airbus", "NL0000235190", "AIR.FRK", "15", "stock")
+    a.add_item("iShares World", "IE00B4L5Y983", "EUNL.FRK", "100", "ETF")
+    a.add_item("iShares EM", "IE00BKM4GZ66", "???", "1000", "ETF")
