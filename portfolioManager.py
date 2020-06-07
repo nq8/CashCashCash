6a12bbda467026b92c1196fc91be97c0d8fa0b42
Nils <nils.quetschlich@gmail.com>
2020-06-07 16:24:12 +0200

    enforced pep8 coding style for portfoliomanager class

--- portfolioManager.py
+++ portfolioManager.py
@@ -2,14 +2,14 @@ import pandas as pd
 import csv
 
 class PortfolioManager:
-    def addItem(self, name, isin, symbol, numShares, category):
+    def add_item(self, name, isin, symbol, num_shares, category):
         with open('portfolio.csv', 'a', newline='') as f:
             newFileWriter = csv.writer(f)
-            newFileWriter.writerow([name, isin, symbol, numShares, category])
-        self.readLocalCSV()
+            newFileWriter.writerow([name, isin, symbol, num_shares, category])
+        self.read_local_csv()
         print(self.df.head())
 
-    def readLocalCSV(self):
+    def read_local_csv(self):
         try:
             self.df = pd.read_csv("portfolio.csv")
         except FileNotFoundError:
@@ -19,11 +19,11 @@ class PortfolioManager:
             self.df = pd.read_csv("portfolio.csv")
 
     def __init__(self):
-        self.readLocalCSV()
+        self.read_local_csv()
         #read current share file if available
 
 
 if __name__ == '__main__':
     a = PortfolioManager()
-    a.addItem("Lufthansa", "DE0008232125", "LHA.FRK", "10", "stock")
-    a.addItem("Airbus", "NL0000235190", "AIR.FRK", "15", "stock")
+    a.add_item("Lufthansa", "DE0008232125", "LHA.FRK", "10", "stock")
+    a.add_item("Airbus", "NL0000235190", "AIR.FRK", "15", "stock")
